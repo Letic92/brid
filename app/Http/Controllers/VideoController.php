0d2cf4411fcce4b888fd2cb3aeb752e621afaa6f
letic92 <letic92@hotmail.com>
2022-10-11 16:20:32 +0200

    Added SEO for page video

--- app/Http/Controllers/VideoController.php
+++ app/Http/Controllers/VideoController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Video;
+use Artesaos\SEOTools\Facades\JsonLd;
+use Artesaos\SEOTools\Facades\OpenGraph;
+use Artesaos\SEOTools\Facades\SEOMeta;
 use Illuminate\Http\Request;
 
 class VideoController extends Controller
@@ -12,6 +15,38 @@ class VideoController extends Controller
         $video = Video::find($request->only('video_id'))->first();
 
         if ($video) {
+            SEOMeta::setTitle('Video | ' . $video->name);
+            OpenGraph::setTitle('Video | ' . $video->name);
+            JsonLd::setTitle('Video | ' . $video->name);
+
+            SEOMeta::addMeta('video:published_time', $video->publish, 'property');
+
+            OpenGraph::addImage($video->image);
+            OpenGraph::addImage(['url' => $video->image, 'size' => 300]);
+            OpenGraph::addImage($video->image, ['height' => 300, 'width' => 300]);
+
+            if (isset($video->descriptio)) {
+                SEOMeta::setDescription($video->descriptio);
+                OpenGraph::setDescription($video->descriptio);
+                JsonLd::setDescription($video->descriptio);
+            } else {
+                SEOMeta::setDescription('Description');
+                OpenGraph::setDescription('Description');
+                JsonLd::setDescription('Description');
+                return view('video', ['video' => $video]);
+            }
+
+            if (isset($video->tags)) {
+                SEOMeta::setKeywords(explode(',', $video->tags));
+            }
+
+            SEOMeta::setCanonical(route('video', ['video_id' => $video->id]));
+            OpenGraph::setUrl(route('video', ['video_id' => $video->id]));
+            OpenGraph::addProperty('type', 'video');
+
+            JsonLd::setType('video');
+            JsonLd::addImage($video->image);
+
             return view('video', ['video' => $video]);
         } else {
             return redirect('/')->with('error', 'Video doesnt exist');
--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -2,9 +2,12 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
-    <title>
-    </title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    {!! SEOMeta::generate() !!}
+    {!! OpenGraph::generate() !!}
+    {!! Twitter::generate() !!}
+    {!! JsonLd::generate() !!}
+    {!! SEO::generate(true) !!}
     <script src="{{ asset('js/app.js') }}"></script>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     @livewireStyles
